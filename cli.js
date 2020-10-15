41d31119f081b4eec8e9a814c12c6509e46f5d1b
Simon Rauch <simon.rauch@towa.at>
2020-10-15 20:21:43 +0200

    more cleanup

--- cli.js
+++ cli.js
@@ -5,13 +5,32 @@ const fs = require("fs");
 
 const args = process.argv.slice(2);
 
-function addAlias(taskAlias) {
+function addAction(taskAlias) {
     addAliases([taskAlias])
 }
 
-function remove(taskAlias) {
-    const output = execSync(`hcl unalias ${taskAlias}`).toString().trim();
-    hclLog(output)
+function removeAction(taskAlias) {
+    verboseHclQuery(`unalias ${taskAlias}`)
+}
+
+function parseAction(file, date, comment) {
+    date = (date) ? date : ""
+    comment = (comment) ? comment : ""
+    const entries = parseCsvFile(file)
+    const aliases = entries.map(entry => entry.task)
+
+    if (addAliases(aliases)) {
+        entries.forEach(entry => {
+            try {
+                verboseHclQuery(`log ${date} @${entry.task} +:${entry.mins} ${comment}`);
+            } catch {
+                harveyError(`Couldn't couldn't log ${entry.task}.`)
+            }
+        })
+    } else {
+        harveyError('Aborting! Something went wrong with generating the task aliases. Please sort them out first before retrying.')
+    }
+
 }
 
 function parseCsvFile(file) {
@@ -29,26 +48,24 @@ function parseCsvFile(file) {
 
 function addAliases(aliases) {
     let errors = 0
-    const hclAliases = execSync(`hcl aliases`).toString().trim().split(', ');
+    const hclAliases = hclQuery('aliases').split(', ');
     const missingAliases = aliases.filter(alias => {
         return !hclAliases.find(hclAlias => hclAlias.includes(alias))
     })
 
     if (missingAliases.length > 0) {
-        const hclTasks = execSync(`hcl tasks`).toString().trim().split(/\r\n|\r|\n/);
+        const hclTasks = hclQuery('hcl tasks').split(/\r\n|\r|\n/);
         missingAliases.forEach(missingAlias => {
             const filteredHclTasks = hclTasks.filter(task => task.includes(missingAlias))
             if (filteredHclTasks.length < 1) {
-                console.log(`HARVEY-ERROR: couldn't create alias ${missingAlias}. Task not found in Harvest.`)
+                harveyError(`Couldn't create alias ${missingAlias}. Task not found in Harvest.`)
                 errors++
             } else if (filteredHclTasks.length > 1) {
-                console.log(`HARVEY-ERROR: couldn't create alias ${missingAlias}. Multiple Tasks found. Please create the alias manually with hcl.`)
+                harveyError(`Couldn't create alias ${missingAlias}. Multiple Tasks found. Please create the alias manually with hcl.`)
                 errors++
             } else {
                 const hclTask = filteredHclTasks[0].split(/\s+/)
-                const projectId = hclTask[0]
-                const taskId = hclTask[1]
-                add(missingAlias, projectId, taskId)
+                add(missingAlias, hclTask[0], hclTask[1])
             }
         })
     }
@@ -62,52 +79,42 @@ function addAliases(aliases) {
 
 function add(alias, projectId, taskId) {
     try {
-        const output = execSync(`hcl alias ${alias} ${projectId} ${taskId}`).toString().trim();
-        hclLog(output);
+        verboseHclQuery(`alias ${alias} ${projectId} ${taskId}`)
     } catch {
-        console.log(`HARVEY-ERROR: couldn't create alias ${alias}`)
+        harveyError(`Couldn't create alias ${alias}.`)
     }
-}
-
-function parse(file, date, comment) {
-    date = (date) ? date : ""
-    comment = (comment) ? comment : ""
-    const entries = parseCsvFile(file)
-    const aliases = entries.map(entry => entry.task)
-
-    if (addAliases(aliases)) {
-        entries.forEach(entry => {
-            try {
-                const command = `hcl log ${date} @${entry.task} +:${entry.mins} ${comment}`;
-                const output = execSync(command).toString().trim();
-                hclLog(output)
-            } catch {
-                console.error(`HARVEY-ERROR: couldn't couldn't log ${entry.task}`)
-            }
-        })
-    } else {
-        console.error('HARVEY-ERROR: Aborting! Something went wrong with generating the task aliases. Please sort them out first before retrying.')
-    }
-
 }
 
 function compain() {
-    console.error('HARVEY-ERROR: unclear instructions... ¯\\_(ツ)_/¯');
+    harveyError('Unclear instructions... ¯\\_(ツ)_/¯');
+}
+
+function hclQuery(qry) {
+    return execSync(`hcl ${qry}`).toString().trim();
+}
+
+function verboseHclQuery(qry) {
+    const result = hclQuery(qry)
+    hclLog(result)
+    return result
 }
 
 function hclLog(log) {
     console.log(`HCL-LOG: ${log}`)
 }
+function harveyError(log) {
+    console.log(`HARVEY-ERROR: ${log}`)
+}
 
 switch (args[0]) {
     case 'add':
-        (args[1]) ? addAlias(args[1]) : compain();
+        (args[1]) ? addAction(args[1]) : compain();
         break;
     case 'remove':
-        (args[1]) ? remove(args[1]) : compain();
+        (args[1]) ? removeAction(args[1]) : compain();
         break;
     case 'parse':
-        (args[1]) ? parse(args[1], args[2], args[3]) : compain();
+        (args[1]) ? parseAction(args[1], args[2], args[3]) : compain();
         break;
     default:
         compain();
