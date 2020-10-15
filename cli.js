7103c2bde2c095b3dc23bd9c3b0e0109f45a5c15
Simon Rauch <simon.rauch@towa.at>
2020-10-15 19:52:02 +0200

    cleanup and error handling... sort of

--- cli.js
+++ cli.js
@@ -6,37 +6,7 @@ const fs = require("fs");
 const args = process.argv.slice(2);
 
 function addAlias(taskAlias) {
-    let taskDetailsLines = 0
-    let taskAliasesLines = 0
-    let taskDetails
-
-    try {
-        taskDetails = execSync(`hcl tasks | grep ${taskAlias}`).toString().trim();
-        taskDetailsLines = taskDetails.split(/\r\n|\r|\n/).length;
-    } catch { }
-
-    try {
-        taskAliasesLines = execSync(`hcl aliases | grep ${taskAlias}`).toString().trim().length;
-    } catch { }
-
-    if (taskDetailsLines == 1 && taskAliasesLines == 0) {
-        const detailsArray = taskDetails.split(/\s+/);
-        const projectId = detailsArray[0];
-        const taskId = detailsArray[1];
-        try {
-            const output = execSync(`hcl alias ${taskAlias} ${projectId} ${taskId}`).toString().trim();
-            hclLog(output);
-        } catch {
-            console.log(`HARVEY-ERROR: couldn't create alias ${taskAlias}`)
-        }
-    } else if (taskDetailsLines == 0 && taskAliasesLines != 0) {
-        console.log(`HARVEY-ERROR: task ${taskAlias} not found`);
-        return false;
-    } else if (taskDetailsLines > 1) {
-        console.log(`HARVEY-ERROR: multiple tasks for ${taskAlias} found, can't decide.`);
-        return false;
-    }
-    return true;
+    addAliases([taskAlias])
 }
 
 function remove(taskAlias) {
@@ -57,22 +27,37 @@ function parseCsvFile(file) {
     return entries
 }
 
-function addAliasesForEntries(entries) {
+function addAliases(aliases) {
+    let errors = 0
     const hclAliases = execSync(`hcl aliases`).toString().trim().split(', ');
-    const missingAliases = entries.filter(entry => {
-        return !hclAliases.find(hclAlias => hclAlias.includes(entry.task))
+    const missingAliases = aliases.filter(alias => {
+        return !hclAliases.find(hclAlias => hclAlias.includes(alias))
     })
 
     if (missingAliases.length > 0) {
         const hclTasks = execSync(`hcl tasks`).toString().trim().split(/\r\n|\r|\n/);
         missingAliases.forEach(missingAlias => {
-            const hclTask = hclTasks.find(task => task.includes(missingAlias.task)).split(/\s+/)
-            const projectId = hclTask[0]
-            const taskId = hclTask[1]
-            add(missingAlias.task, projectId, taskId)
+            const filteredHclTasks = hclTasks.filter(task => task.includes(missingAlias))
+            if (filteredHclTasks.length < 1) {
+                console.log(`HARVEY-ERROR: couldn't create alias ${missingAlias}. Task not found in Harvest.`)
+                errors++
+            } else if (filteredHclTasks.length > 1) {
+                console.log(`HARVEY-ERROR: couldn't create alias ${missingAlias}. Multiple Tasks found. Please create the alias manually with hcl.`)
+                errors++
+            } else {
+                const hclTask = filteredHclTasks[0].split(/\s+/)
+                const projectId = hclTask[0]
+                const taskId = hclTask[1]
+                add(missingAlias, projectId, taskId)
+            }
         })
     }
 
+    if (errors > 0) {
+        return false
+    }
+
+    return true
 }
 
 function add(alias, projectId, taskId) {
@@ -88,17 +73,22 @@ function parse(file, date, comment) {
     date = (date) ? date : ""
     comment = (comment) ? comment : ""
     const entries = parseCsvFile(file)
-    addAliasesForEntries(entries)
+    const aliases = entries.map(entry => entry.task)
 
-    entries.forEach(entry => {
-        try {
-            const command = `hcl log ${date} @${entry.task} +:${entry.mins} ${comment}`;
-            const output = execSync(command).toString().trim();
-            hclLog(output)
-        } catch {
-            console.error(`HARVEY-ERROR: couldn't couldn't log ${entry.task}`)
-        }
-    })
+    if (addAliases(aliases)) {
+        entries.forEach(entry => {
+            try {
+                const command = `hcl log ${date} @${entry.task} +:${entry.mins} ${comment}`;
+                const output = execSync(command).toString().trim();
+                hclLog(output)
+            } catch {
+                console.error(`HARVEY-ERROR: couldn't couldn't log ${entry.task}`)
+            }
+        })
+    } else {
+        console.error('HARVEY-ERROR: Aborting! Something went wrong with generating the task aliases. Please sort them out first before retrying.')
+    }
+
 }
 
 function compain() {
