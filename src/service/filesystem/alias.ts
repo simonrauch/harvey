b0d8b4056b51ec56e168ab45758ed25bb056f444
Simon Rauch <sr@simonrauch.com>
2022-03-17 20:21:04 +0100

    refactor alias file system service

--- src/service/filesystem/alias.ts
+++ src/service/filesystem/alias.ts
@@ -1,27 +1,16 @@
-import fs from 'fs';
-import path from 'path';
-import { transformPath } from '.';
+import { fileExists, readFromJsonFile, transformPath, writeToJsonFile } from '.';
 import { Alias } from '../../business/alias';
 import { HarveyConfig } from '../../business/config';
 
 export function readAliasFile(): Map<string, Alias> {
-  let filePath = HarveyConfig.getConfig().aliasFilePath;
-  if (!aliasFileExists()) {
+  const filePath = HarveyConfig.getConfig().aliasFilePath;
+  if (!fileExists(filePath)) {
     writeAliasFile(new Map());
   }
-  filePath = transformPath(filePath);
-  return new Map(JSON.parse(fs.readFileSync(filePath).toString()));
+  return new Map(readFromJsonFile(filePath));
 }
 
 export function writeAliasFile(aliases: Map<string, Alias>): void {
   const filePath = transformPath(HarveyConfig.getConfig().aliasFilePath);
-  if (!fs.existsSync(path.dirname(filePath))) {
-    fs.mkdirSync(path.dirname(filePath), { recursive: true });
-  }
-  fs.writeFileSync(filePath, JSON.stringify(Array.from(aliases.entries())), 'utf8');
-}
-
-function aliasFileExists(): boolean {
-  const filePath = transformPath(HarveyConfig.getConfig().aliasFilePath);
-  return fs.existsSync(filePath);
+  writeToJsonFile(Array.from(aliases.entries()), filePath);
 }
