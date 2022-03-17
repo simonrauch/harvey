d5b7e21a1330b85bbc752c8f0de45df58fa17b5e
Simon Rauch <simon.rauch@towa.at>
2022-03-17 22:10:15 +0100

    refactor config filesystem service

--- src/service/filesystem/config.ts
+++ src/service/filesystem/config.ts
@@ -1,24 +1,13 @@
-import fs from 'fs';
-import path from 'path';
-import { transformPath } from '.';
+import { fileExists, readFromJsonFile, writeToJsonFile } from '.';
 import { Config } from '../../business/config';
 
-export function configFileExists(filePath: string): boolean {
-  filePath = transformPath(filePath);
-  return fs.existsSync(filePath);
-}
 export function readConfigFile(filePath: string): Config {
-  if (!configFileExists(filePath)) {
-    throw new Error(`No config file found at ${filePath}. Please create one, by running the init command`);
+  if (!fileExists(filePath)) {
+    throw new Error(`No config file found at "${filePath}". Please create one, by running the init command`);
   }
-  filePath = transformPath(filePath);
-  return JSON.parse(fs.readFileSync(filePath).toString());
+  return readFromJsonFile(filePath);
 }
 
 export function writeConfigFile(config: Config, filePath: string): void {
-  filePath = transformPath(filePath);
-  if (!fs.existsSync(path.dirname(filePath))) {
-    fs.mkdirSync(path.dirname(filePath), { recursive: true });
-  }
-  fs.writeFileSync(filePath, JSON.stringify(config, null, 2), 'utf8');
+  writeToJsonFile(config, filePath);
 }
