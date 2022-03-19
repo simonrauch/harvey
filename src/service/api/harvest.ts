90c76c58a9220da7a901271ff9575bfe6431cf8b
Simon Rauch <simonrauch@users.noreply.github.com>
2022-03-19 02:31:46 +0100

    fix: remove node harvest api module and replace with axios calls (#28)
    * fix: remove node harvest api module and replace with axios calls
    
    * test: added harvest service tests

--- src/service/api/harvest.ts
+++ src/service/api/harvest.ts
@@ -1,49 +1,50 @@
 import { HarveyConfig } from '../../business/config';
-import Harvest from 'node-harvest-api';
 import { HarveyError } from '../../business/error';
-import {
-  HarvestProjectAssignment,
-  HarvestProjectTaskAssignment,
-  HarvestTimeEntry,
-  HarvestUser,
-} from '../../business/harvest';
+import axios from 'axios';
+import { HarvestProjectAssignment, HarvestProjectTaskAssignment, HarvestTimeEntry } from '../../business/harvest';
 
 let projectTaskAssignmentCache: Array<HarvestProjectTaskAssignment> | null;
 let projectAssignmentCache: Array<HarvestProjectAssignment> | null;
 let authenticatedUserIdCache: number | null;
-let harvestSdk: Harvest | null;
 
 export function resetHarvestCache(): void {
   projectTaskAssignmentCache = null;
   projectAssignmentCache = null;
   authenticatedUserIdCache = null;
-  harvestSdk = null;
 }
 
-export function getHarvestSdk(): Harvest {
-  if (!harvestSdk) {
-    const config = HarveyConfig.getConfig();
-    harvestSdk = new Harvest(config.accountId, config.accessToken, 'harvey');
-  }
+function getRequestHeaders(): object {
+  const config = HarveyConfig.getConfig();
+  return {
+    'Harvest-Account-Id': config.accountId,
+    Authorization: `Bearer ${config.accessToken}`,
+    'User-Agent': 'Harvey',
+  };
+}
 
-  return harvestSdk;
+function getAxiosRequestConfig(): object {
+  return {
+    headers: getRequestHeaders(),
+  };
+}
+
+function getBaseUrl(): string {
+  return `https://${HarveyConfig.getConfig().harvestSubdomain ?? 'api'}.harvestapp.com/api/v2`;
 }
 
 export async function getMyProjectAssignments(forceFetch = false): Promise<HarvestProjectAssignment[]> {
-  return new Promise((resolve) => {
+  return new Promise((resolve, reject) => {
     if (projectAssignmentCache && !forceFetch) {
       resolve(projectAssignmentCache);
+      return;
     }
-
-    const harvest = getHarvestSdk();
-
-    harvest.users
-      .pipe('me')
-      .project_assignments.all()
-      .then((projectAssignments: HarvestProjectAssignment[]) => {
-        projectAssignmentCache = projectAssignments;
-        resolve(projectAssignmentCache);
-      });
+    axios
+      .get(getBaseUrl() + '/users/me/project_assignments', getAxiosRequestConfig())
+      .then((res) => {
+        projectAssignmentCache = res.data.project_assignments;
+        resolve(res.data.project_assignments);
+      })
+      .catch(reject);
   });
 }
 
@@ -51,6 +52,7 @@ export async function getMyProjectTaskAssignments(forceFetch = false): Promise<A
   return new Promise((resolve) => {
     if (projectTaskAssignmentCache && !forceFetch) {
       resolve(projectTaskAssignmentCache);
+      return;
     }
     getMyProjectAssignments(forceFetch).then((projectAssignments) => {
       const projectTaskAssignments: HarvestProjectTaskAssignment[] = [];
@@ -70,92 +72,138 @@ export async function getMyProjectTaskAssignments(forceFetch = false): Promise<A
 
 export async function getRunningTimeEntry(): Promise<HarvestTimeEntry | null> {
   return new Promise((resolve, reject) => {
-    const harvest = getHarvestSdk();
     getAuthenticatedUserId().then((userId) => {
-      harvest.time_entries.get({ is_running: true, user_id: userId }).then((timeEntries: HarvestTimeEntry[]) => {
-        if (timeEntries.length > 1) {
-          reject(
-            new HarveyError(
-              'You somehow managed to start multiple time entries. Only one time entry should be running. Please stop them and restart only the correct one.',
-            ),
-          );
-          return;
-        }
-        if (timeEntries[0]) {
-          resolve(timeEntries[0]);
-        }
-        resolve(null);
-      });
+      axios
+        .get(getBaseUrl() + '/time_entries', {
+          params: { is_running: true, user_id: userId },
+          ...getAxiosRequestConfig(),
+        })
+        .then((res) => {
+          if (res.data.time_entries.length > 1) {
+            reject(
+              new HarveyError(
+                'You somehow managed to start multiple time entries. Only one time entry should be running. Please stop them and restart only the correct one.',
+              ),
+            );
+            return;
+          }
+          if (res.data.time_entries[0]) {
+            resolve(res.data.time_entries[0]);
+          }
+          resolve(null);
+        })
+        .catch(reject);
     });
   });
 }
+
 export async function saveTimeEntry(timeEntry: HarvestTimeEntry): Promise<HarvestTimeEntry> {
-  return new Promise((resolve) => {
-    const harvest = getHarvestSdk();
+  return new Promise((resolve, reject) => {
     if (timeEntry.id) {
-      harvest.time_entries.update(timeEntry.id, timeEntry).then(resolve);
+      axios
+        .patch(getBaseUrl() + '/time_entries/' + timeEntry.id, timeEntry, getAxiosRequestConfig())
+        .then((res) => {
+          resolve(res.data);
+        })
+        .catch(reject);
     } else {
-      harvest.time_entries.create(timeEntry).then(resolve);
+      axios
+        .post(getBaseUrl() + '/time_entries', timeEntry, getAxiosRequestConfig())
+        .then((res) => {
+          resolve(res.data);
+        })
+        .catch(reject);
     }
   });
 }
 
 export async function stopTimeEntry(timeEntry: HarvestTimeEntry): Promise<HarvestTimeEntry> {
-  return new Promise((resolve) => {
-    const harvest = getHarvestSdk();
-    if (timeEntry.id) {
-      harvest.time_entries.stop(timeEntry.id).then(resolve);
-    } else {
-      resolve(timeEntry);
-    }
-  });
-}
-export async function restartTimeEntry(timeEntry: HarvestTimeEntry): Promise<HarvestTimeEntry> {
-  return new Promise((resolve) => {
-    const harvest = getHarvestSdk();
-    if (timeEntry.id) {
-      harvest.time_entries.restart(timeEntry.id).then(resolve);
-    } else {
-      resolve(timeEntry);
-    }
-  });
-}
-export async function getAuthenticatedUserId(): Promise<number> {
-  return new Promise((resolve) => {
-    if (authenticatedUserIdCache) {
-      resolve(authenticatedUserIdCache);
-    }
-    const harvest = getHarvestSdk();
-    harvest.users.me().then((user: HarvestUser) => {
-      authenticatedUserIdCache = user.id;
-      resolve(authenticatedUserIdCache);
-    });
-  });
-}
-export async function getMyTimeEntriesPerDate(date: string): Promise<HarvestTimeEntry[]> {
-  return new Promise((resolve) => {
-    const harvest = getHarvestSdk();
-    getAuthenticatedUserId().then((userId) => {
-      harvest.time_entries.get({ from: date, to: date, user_id: userId }).then(resolve);
-    });
-  });
-}
-export async function deleteTimeEntry(timeEntry: HarvestTimeEntry): Promise<void> {
-  return new Promise((resolve) => {
-    const harvest = getHarvestSdk();
+  return new Promise((resolve, reject) => {
     if (!timeEntry.id) {
-      throw new Error('id property has to be set, to delete a time entry.');
+      reject(new Error('id property has to be set, to stop a time entry.'));
+      return;
     }
-    harvest.time_entries.delete(timeEntry.id).then(resolve);
+    axios
+      .patch(getBaseUrl() + '/time_entries/' + timeEntry.id + '/stop', {}, getAxiosRequestConfig())
+      .then((res) => {
+        resolve(res.data);
+      })
+      .catch(reject);
   });
 }
+
+export async function restartTimeEntry(timeEntry: HarvestTimeEntry): Promise<HarvestTimeEntry> {
+  return new Promise((resolve, reject) => {
+    if (!timeEntry.id) {
+      reject(new Error('id property has to be set, to restart a time entry.'));
+      return;
+    }
+    axios
+      .patch(getBaseUrl() + '/time_entries/' + timeEntry.id + '/restart', {}, getAxiosRequestConfig())
+      .then((res) => {
+        resolve(res.data);
+      })
+      .catch(reject);
+  });
+}
+
+export async function getAuthenticatedUserId(forceFetch = false): Promise<number> {
+  return new Promise((resolve, reject) => {
+    if (authenticatedUserIdCache && !forceFetch) {
+      resolve(authenticatedUserIdCache);
+      return;
+    }
+    axios
+      .get(getBaseUrl() + '/users/me', getAxiosRequestConfig())
+      .then((res) => {
+        authenticatedUserIdCache = res.data.id;
+        resolve(res.data.id);
+      })
+      .catch(reject);
+  });
+}
+
+export async function getMyTimeEntriesPerDate(date: string): Promise<HarvestTimeEntry[]> {
+  return new Promise((resolve, reject) => {
+    getAuthenticatedUserId().then((userId) => {
+      axios
+        .get(getBaseUrl() + '/time_entries', {
+          params: { from: date, to: date, user_id: userId },
+          ...getAxiosRequestConfig(),
+        })
+        .then((res) => {
+          resolve(res.data.time_entries);
+        })
+        .catch(reject);
+    });
+  });
+}
+
+export async function deleteTimeEntry(timeEntry: HarvestTimeEntry): Promise<void> {
+  return new Promise((resolve, reject) => {
+    if (!timeEntry.id) {
+      reject(new Error('id property has to be set, to delete a time entry.'));
+      return;
+    }
+    axios
+      .delete(getBaseUrl() + '/time_entries/' + timeEntry.id, getAxiosRequestConfig())
+      .then(() => resolve())
+      .catch(reject);
+  });
+}
+
 export async function isAccountIdAndTokenValid(accountId: string, token: string): Promise<boolean> {
   return new Promise((resolve) => {
-    const harvest = new Harvest(accountId, token, 'harvey');
-    harvest.users
-      .me()
-      .then((user: HarvestUser) => {
-        if (user.first_name) {
+    axios
+      .get(getBaseUrl() + '/users/me', {
+        headers: {
+          'Harvest-Account-Id': accountId,
+          Authorization: `Bearer ${token}`,
+          'User-Agent': 'Harvey',
+        },
+      })
+      .then((res) => {
+        if (res.data.first_name) {
           resolve(true);
           return;
         }
--- src/types/node-harvest-api/index.d.ts
+++
@@ -1,7 +0,0 @@
-declare module 'node-harvest-api' {
-  export default class Harvest {
-    constructor(accountId: string, accessToken: string, applicationName: string);
-    time_entries: function;
-    users: function;
-  }
-}
